96885860feea19eeb77ff85a54c32b72f68b23d1
gabriel <g.carvajal.pizarro@gmail.com>
2021-07-14 20:52:26 -0400

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import HeoresApp from './HeoresApp';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/js/bootstrap.js';
 import 'animate.css/animate.min.css';
 
 ReactDOM.render(
