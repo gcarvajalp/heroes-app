98f875c7322dace46ff069aafc7130932eb35959
gabriel <g.carvajal.pizarro@gmail.com>
2021-07-14 21:58:45 -0400

    added home info

--- src/components/home/HomeScreen.js
+++ src/components/home/HomeScreen.js
@@ -3,12 +3,12 @@ import React from 'react'
 const HomeScreen = () => {
   return (
     <header className="masthead">
-      <div className="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+      <div className="container d-flex h-100 align-items-center justify-content-center">
         <div className="d-flex justify-content-center">
           <div className="text-center">
-            <img src="./hero.svg" width="150" height="150" class="d-inline-block align-top" alt=""></img>
-            <h2 className="mx-auto mt-2 mb-5">A superhero catalog created with React.js by <a href="">Gabriel Carvajal</a></h2>
-            <a className="btn btn-primary" href="#about">Github code!</a>
+            <img src="./hero.svg" width="140" height="140" class="d-inline-block align-top" alt=""></img>
+            <h2 className="mx-auto mt-2 mb-5">A superhero catalog created with React.js by <a href="https://twitter.com/gabo_carvajal" target="_blank">Gabriel Carvajal</a></h2>
+            <a className="btn btn-primary" href="https://github.com/gcarvajalp/herores-app" target="_blank">Github code!</a>
           </div>
         </div>
       </div>
