2ad24363fd3a3ef3082092a0d78d2936b4ec5288
gabriel <g.carvajal.pizarro@gmail.com>
2021-07-15 00:05:16 -0400

    .

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,61 +1,72 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+
 import { heroes } from '../../data/heroes';
 import { useForm } from '../../hooks/useForm';
 import HeroCard from '../heroes/HeroCard';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
 
 
-const SearchScreen = () => {
+const SearchScreen = ({ history }) => {
 
-  const heroesFiltered = heroes;
+  const location = useLocation();
+  const { q = '' } = queryString.parse(location.search);
 
   const [formValues, handleInputChange] = useForm({
-    search: ''
+    search: q
   });
 
   const { search } = formValues;
 
+  const heroesFiltered = getHeroesByName(search);
+
+  useEffect(() => {
+    console.log('useEffect');
+  }, [])
+
   const handleSearch = (e) => {
     e.preventDefault();
+    history.push(`?q=${search}`)
   }
 
   return (
     <div>
-      <h1>Search</h1>
+      <h1>Search... {search}</h1>
       <hr />
       {/* <div className="row">
         <div className="col-12">
           <form onSubmit={handleSearch}>
             <div className="">
 
-            <input
-              type="text"
-              placeholder="Find your hero"
-              className="form-control"
-              name="search"
-              autoComplete="off"
-              value={search}
-              onChange={handleInputChange}
-            />
-            <button
-              type="submit"
-              className="btn btn-outline-primary">
-              Seach...
-            </button>
+              <input
+                type="text"
+                placeholder="Find your hero"
+                className="form-control"
+                name="search"
+                autoComplete="off"
+                value={search}
+                onChange={handleInputChange}
+              />
+              <button
+                type="submit"
+                className="btn btn-outline-primary">
+                Seach...
+              </button>
             </div>
           </form>
         </div>
       </div> */}
       <div className="card-columns animate__animated animate__fadeIn">
-          
-          {
-            heroesFiltered.map(hero => (
-              <HeroCard
-                key={hero.id}
-                {...hero}
-              />
-            ))
-          }
+
+        {
+          heroesFiltered.map(hero => (
+            <HeroCard
+              key={hero.id}
+              {...hero}
+            />
+          ))
+        }
 
       </div>
     </div>
--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useHistory, useLocation, Redirect } from 'react-router-dom';
+
+import { useForm } from '../../hooks/useForm';
 
 
 const Navbar = () => {
+
+  const history = useHistory();
+
+  const [formValues, handleInputChange, reset] = useForm({
+    search: ''
+  });
+
+  const { search } = formValues;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    history.push(`search?q=${search}`);
+    reset();
+  }
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link
@@ -45,9 +62,23 @@ const Navbar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
         <ul className="navbar-nav ml-auto">
-          <form className="form-inline">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-            <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+          <form
+            className="form-inline"
+            onSubmit={handleSubmit}
+          >
+            <input
+              class="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              name="search"
+              value={search}
+              onChange={handleInputChange}
+            ></input>
+            <button
+              class="btn btn-outline-primary my-2 my-sm-0"
+              type="submit"
+            >Search</button>
           </form>
         </ul>
       </div>
